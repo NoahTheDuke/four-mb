f0883b83fdd5bbdd6eb3b330951ef10335b5eb94
Noah Bogart <noah.bogart@hey.com>
2021-06-11 00:06:14 -0400

    Clean up asset names

--- assets/dungeon-blocks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="dungeon-blocks" tilewidth="16" tileheight="16" tilecount="56" columns="8">
- <image source="dungeon-tiles.png" width="128" height="112"/>
-</tileset>

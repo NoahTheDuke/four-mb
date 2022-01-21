75c3d4794343219325fed5ba88a92c2a32c63fce
Noah Bogart <noah.bogart@hey.com>
2022-01-21 14:43:22 -0500

    final broken commit

--- convert.clj
+++ convert.clj
@@ -1,8 +1,29 @@
 (ns convert
-  (:require [babashka.fs :as fs]
-            [clojure.java.shell :refer [sh]]))
+  (:require [net.cgrand.xforms :as x])
+  ; (:require [babashka.fs :as fs]
+  ;           [clojure.java.shell :refer [sh]])
+  )
 
-(def files (map str (fs/glob "assets/dungeon-1" "*.json")))
+; (def files (map str (fs/glob "assets/dungeon-1" "*.json")))
 
-(doseq [filepath files]
-  (sh "jj" "-u" "-i" filepath "-o" filepath))
+; (doseq [filepath files]
+;   (sh "jj" "-u" "-i" filepath "-o" filepath))
+
+
+(transduce
+  (comp
+    (x/for [i %
+            j (range i)]
+      (* i j))
+    (filter #(< % 129))
+    )
+  + 0
+  (range 256))
+
+(reduce
+  + 0
+  (for [
+        ; i (range 256)
+        i (filter #(< % 129) (range 256))
+        j (range i)]
+    (* i j)))
